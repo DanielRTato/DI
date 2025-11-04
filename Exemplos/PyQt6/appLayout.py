bac51a1662d2a6409e6cf05a072d191d6d86e1db
DanielRTato <drodrigueztato@danielcastelao.org>
2025-11-04 20:45:16 +0100

    Vertical layout

--- Exemplos/PyQt6/appLayout.py
+++ Exemplos/PyQt6/appLayout.py
@@ -10,18 +10,25 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.setWindowTitle("My App")
 
-        #layout = QVBoxLayout() # Horizontal
-        layout = QHBoxLayout()  # Vertical
+        layout1 = QHBoxLayout()
+        layout2 = QVBoxLayout()
+        layout3 = QVBoxLayout()
 
+        layout2.addWidget(Color("red"))
+        layout2.addWidget(Color("yellow"))
+        layout2.addWidget(Color("purple"))
 
-        layout.addWidget(Color("red"))
-        layout.addWidget(Color("green"))
-        layout.addWidget(Color("orange"))
-        layout.addWidget(Color("blue"))
+        layout1.addLayout(layout2)
 
+        layout1.addWidget(Color("green"))
+
+        layout3.addWidget(Color("red"))
+        layout3.addWidget(Color("purple"))
+
+        layout1.addLayout(layout3)
 
         widget = QWidget()
-        widget.setLayout(layout)
+        widget.setLayout(layout1)
         self.setCentralWidget(widget)
 
 
